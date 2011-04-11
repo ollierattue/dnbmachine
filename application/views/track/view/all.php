a61f97523e7d5d6bfea068dd127c8978ef161dee
Ollie Rattue <orattue@toomanytabs.com>
2011-04-11 19:53:10 +0200

    Changed list all tracks to a list format

--- application/views/track/view/all.php
+++ application/views/track/view/all.php
@@ -3,15 +3,17 @@
 <?php if ($results->num_rows() == 0): ?>
 Nothing is ready to roll. Try another category.
 <?php else: ?>
+<ul>
 <?php foreach ($results->result() as $row): ?>
 
-<h3><a href="<?php echo base_url()."track/edit/{$row->track_url_slug}";?>"><?php echo $row->artist_name; ?> - <?php echo $row->track_name; ?></a> - 
+<li><a href="<?php echo base_url()."track/edit/{$row->track_url_slug}";?>"><?php echo $row->artist_name; ?> - <?php echo $row->track_name; ?></a> - 
 <?php if ($row->track_soundcloud_id): ?>
 Soundcloud
 <?php elseif ($row->track_youtube_id): ?>
 YouTube
 <?php endif; ?>
-</h3>
+</li>
 
 <?php endforeach; ?>
+</ul>
 <?php endif; ?>
