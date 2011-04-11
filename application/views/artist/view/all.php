443b06d33ce16940248e770c865e6b236268b07d
Ollie Rattue <orattue@toomanytabs.com>
2011-04-11 10:45:41 +0200

    Changed delete icon to a png to get transparency

--- application/views/artist/view/all.php
+++ application/views/artist/view/all.php
@@ -6,7 +6,7 @@ Nothing is ready to roll. Try another category.
 <ul>
 <?php foreach ($results->result() as $row): ?>
 
-<li><a href="<?php echo base_url(); ?>artist/edit/<?php echo $row->artist_url_slug; ?>"><?php echo $row->artist_name; ?></a> <a href="<?php echo base_url(); ?>track/delete/<?php echo $row->artist_url_slug; ?>"><img src="<?php echo base_url();?>images/icons/delete.gif" alt="delete artist"  onclick="if ( confirm('Are you sure you want to delete this artist and all their tracks?') ) { return true;}return false;" /></a></li>
+<li><a href="<?php echo base_url(); ?>artist/edit/<?php echo $row->artist_url_slug; ?>"><?php echo $row->artist_name; ?></a> <a href="<?php echo base_url(); ?>track/delete/<?php echo $row->artist_url_slug; ?>"><img src="<?php echo base_url();?>images/icons/delete.png" alt="delete artist"  onclick="if ( confirm('Are you sure you want to delete this artist and all their tracks?') ) { return true;}return false;" style="float:right;" /></a></li>
 <?php endforeach; ?>
 </ul>
 <?php endif; ?>
