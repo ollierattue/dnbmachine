64c2f8b6810db9cd7a0574747fb680aa517c0970
Ollie Rattue <orattue@toomanytabs.com>
2011-04-12 16:33:59 +0200

    Added analytics code to footer

--- application/views/templates/frontend/footer.php
+++ application/views/templates/frontend/footer.php
@@ -23,5 +23,25 @@
 									<?php endif; ?>
                           </div>
                   </div>
+
+
+				<?php
+				// only output google analytic code live site
+				if ("${_SERVER['HTTP_HOST']}" == 'dnbmachine.com'):
+				?>
+				<script type="text/javascript">
+
+				  var _gaq = _gaq || [];
+				  _gaq.push(['_setAccount', 'UA-7844413-7']);
+				  _gaq.push(['_trackPageview']);
+
+				  (function() {
+				    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+				  })();
+
+				</script>
+				<?php endif; ?>
         </body>
 </html>
